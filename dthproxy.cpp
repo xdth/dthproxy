85ad98a5d1a2cbd6870b3cf45548b0625ad479c0
xdth <info@dthlabs.com>
2022-04-14 21:10:12 -0400

    Commenting

--- dthproxy.cpp
+++ dthproxy.cpp
@@ -2,7 +2,10 @@
  * Quebec, April 12, 2020 - dth [at] dthlabs [dot] com    *
 **********************************************************/
 
+
+//--------------------------------------------------------------------
 // ToDo
+//
 // - check if the user is root
 // - help file
 // - alternative args (eg.: -h or --help)
@@ -12,14 +15,76 @@
 //   - it's opening and closing the file at every iteration
 //   - it should ignore lines starting with #
 //   - throw error if config file does not exist
-// - refactor stringExplode(), add delimiter as parameter
-// - hash maps for reading the config file
+// - refactor stringExplode()
+//   - add delimiter as parameter
+//   - hash maps for reading the config file
 
+
+//--------------------------------------------------------------------
+// Includes
 #include <iostream>
 #include <fstream>
 
+
+//--------------------------------------------------------------------
+// Config variables
 std::string input_interface, input_interface_ipcidr, output_interface;
 
+
+//--------------------------------------------------------------------
+// Function prototypes
+std::string* stringExplode(const char* stringToBeExploded);
+void readConfigFile();
+
+
+//--------------------------------------------------------------------
+// Main
+int main(int argc, char *argv[]) {
+  std::string clargs[7]; // command line arguments
+  
+  std::cout << "\n\n*** activating proxy\n\n";
+
+  // if there are command line arguments
+  if(argc == 7) {
+    for (int i = 0; i < argc; i++) {
+      clargs[i] = argv[i];
+
+      if (clargs[i] == "-input_interface") {
+        input_interface = argv[++i];
+      }
+      if (clargs[i] == "-input_interface_ipcidr") {
+        input_interface_ipcidr = argv[++i];
+      }
+      if (clargs[i] == "-output_interface") {
+        output_interface = argv[++i];
+      }
+    }
+  }
+  else if(argc > 1 && argc < 7 || argc > 7) {
+    clargs[1] = argv[1];
+
+    if (clargs[1] == "--help") {
+      std::cout << "This is the help section. Bla bla bla.\n";
+    } else {
+      std::cout << "Please enter 3 arguments. Or try: dthproxy --help.\n";
+    }
+  }
+  else {
+    // No args entered. Will read from the config file.
+    readConfigFile();
+  }
+
+  // test to see if the config variables were setup correctly from the file or from the commandline args
+  std::cout << "input_interface: " << input_interface << "\n";
+  std::cout << "input_interface_ipcidr: " << input_interface_ipcidr << "\n";
+  std::cout << "output_interface: " << output_interface << "\n"; 
+  
+  return 0;
+}
+
+
+//--------------------------------------------------------------------
+// Function stringExplode()
 // Return an array containing the exploded strings
 std::string* stringExplode(const char* stringToBeExploded){
   std::string str = stringToBeExploded;
@@ -58,6 +123,10 @@ std::string* stringExplode(const char* stringToBeExploded){
   return result;
 }
 
+
+//--------------------------------------------------------------------
+// Function readConfigFile()
+// Read the config file and set the config variables
 void readConfigFile() {
   char *textFromConfigFile;
   std::string stringFromConfigFile = textFromConfigFile; // casting char* to string
@@ -85,46 +154,4 @@ void readConfigFile() {
   }
 
   readFromConfigFile.close();   
-}
-
-int main(int argc, char *argv[]) {
-  std::string clargs[7]; // command line arguments
-  
-  std::cout << "\n\n*** activating proxy\n\n";
-
-  if(argc == 7) {
-    for (int i = 0; i < argc; i++) {
-      clargs[i] = argv[i];
-
-      if (clargs[i] == "-input_interface") {
-        input_interface = argv[++i];
-      }
-      if (clargs[i] == "-input_interface_ipcidr") {
-        input_interface_ipcidr = argv[++i];
-      }
-      if (clargs[i] == "-output_interface") {
-        output_interface = argv[++i];
-      }
-    }
-  }
-  else if(argc > 1 && argc < 7 || argc > 7) {
-    clargs[1] = argv[1];
-
-    if (clargs[1] == "--help") {
-      std::cout << "This is the help section. Bla bla bla.\n";
-    } else {
-      std::cout << "Please enter 3 arguments. Or try: dthproxy --help.\n";
-    }
-  }
-  else {
-    // No args entered. Will read from the config file.
-    readConfigFile();
-  }
-
-  // test to see if the config variables were setup correctly from the file or from the commandline args
-  std::cout << "input_interface: " << input_interface << "\n";
-  std::cout << "input_interface_ipcidr: " << input_interface_ipcidr << "\n";
-  std::cout << "output_interface: " << output_interface << "\n"; 
-  
-  return 0;
 }
