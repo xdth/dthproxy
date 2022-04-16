ae8f91e87f5bc72b7128e1996fc21e477fbe891b
xdth <info@dthlabs.com>
2022-04-16 14:08:38 -0400

    Error on casting char* to string

--- dthproxy.cpp
+++ dthproxy.cpp
@@ -31,7 +31,9 @@
 //--------------------------------------------------------------------
 // Config variables
 
-std::string input_interface, input_interface_ipcidr, output_interface = "";
+std::string input_interface;
+std::string input_interface_ipcidr;
+std::string output_interface;
 
 
 //--------------------------------------------------------------------
@@ -69,21 +71,21 @@ int main(int argc, char *argv[]) {
         output_interface = argv[++i];
       }
       else {
-        if (clargs[i] != clargs[0]) { // if wrong argument is not the executable's name
-          std::cout << "Invalid option: " << clargs[i] << "\n";
-          return 1;
-        }
+        // if (clargs[i] != clargs[0]) { // if wrong argument is not the executable's name
+        //   std::cout << "Invalid option: " << clargs[i] << "\n";
+        //   return 1;
+        // }
       }
     }
   }
   else if(argc > 1 && argc < 7 || argc > 7) {
-    clargs[1] = argv[1];
+    // clargs[1] = argv[1];
 
-    if (clargs[1] == "--help" || clargs[1] == "-h") {
-      help();
-    } else {
-      std::cout << "Please enter 3 arguments. Or try: dthproxy --help.\n";
-    }
+    // if (clargs[1] == "--help" || clargs[1] == "-h") {
+    //   help();
+    // } else {
+    //   std::cout << "Please enter 3 arguments. Or try: dthproxy --help.\n";
+    // }
   }
   else {
     // No args entered. Will read from the config file.
@@ -160,10 +162,10 @@ std::string* explodeString(const char* string_to_be_exploded){
 
 void readConfigFile() {
   char *text_from_config_file;
-  std::string string_from_config_file = text_from_config_file; // casting char* to string
+  std::string string_from_config_file = std::string(text_from_config_file); // casting char* to string
   std::ifstream read_from_config_file("dthproxy_config.cfg");
 
-  // int counter = 0;
+  int counter = 0;
   // while (getline (read_from_config_file, string_from_config_file)) {
   //   std::string* result_array = explodeString(string_from_config_file.c_str());
 
@@ -184,7 +186,7 @@ void readConfigFile() {
   //   counter++;
   // }
 
-  // read_from_config_file.close();
+  read_from_config_file.close();
 }
 
 
